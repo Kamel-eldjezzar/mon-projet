8dee62158185855e0640468e33c9f405725870e5
Kamel <eldjezzarkamel@gmail.com>
2025-07-26 00:17:22 +0100

    Ajout du projet Calculatrice en C

--- calculatrice.c
+++ calculatrice.c
@@ -1,1 +1,34 @@
-// fichier calculatrice
+#include <stdio.h>
+
+int main() {
+    char op;
+    double num1, num2;
+
+    printf("Entrez une opération (+, -, *, /): ");
+    scanf(" %c", &op);
+
+    printf("Entrez deux nombres: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    switch(op) {
+        case '+':
+            printf("Résultat: %.2lf\n", num1 + num2);
+            break;
+        case '-':
+            printf("Résultat: %.2lf\n", num1 - num2);
+            break;
+        case '*':
+            printf("Résultat: %.2lf\n", num1 * num2);
+            break;
+        case '/':
+            if(num2 != 0)
+                printf("Résultat: %.2lf\n", num1 / num2);
+            else
+                printf("Erreur : division par zéro !\n");
+            break;
+        default:
+            printf("Opérateur non valide !\n");
+    }
+
+    return 0;
+}// fichier calculatrice
